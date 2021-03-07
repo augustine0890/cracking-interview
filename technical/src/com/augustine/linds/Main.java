30f13c5426a04529178a3c2e79ca7bde611f80f0
augustine <augustino0890@gmail.com>
2021-03-07 21:48:54 +0900

    swap elements in linked list

--- technical/src/com/augustine/linds/Main.java
+++ technical/src/com/augustine/linds/Main.java
@@ -1,6 +1,59 @@
 package com.augustine.linds;
 
+
 public class Main {
+    // Swapping Elements in a Linked List
+    public static void swapNodes(LinkedList list, String data1, String data2) {
+        System.out.println("Swapping " + data1 + " with " + data2);
+
+        Node node1Prev = null;
+        Node node2Prev = null;
+        Node node1 = list.head;
+        Node node2 = list.head;
+
+        if (data1 == data2) {
+            System.out.println("Elements are the same - no swap to be made");
+            return;
+        }
+
+        while (node1 != null) {
+            if (node1.data.equals(data1)) {
+                break;
+            }
+            node1Prev = node1;
+            node1 = node1.getNextNode();
+        }
+
+        while (node2 != null) {
+            if (node2.data.equals(data2)) {
+                break;
+            }
+            node2Prev = node2;
+            node2 = node2.getNextNode();
+        }
+
+        if (node1 == null || node2 == null) {
+            System.out.println("Swap not possible - one or more element is not in the list");
+            return;
+        }
+
+        if (node1Prev == null) {
+            list.head = node2;
+        } else {
+            node1Prev.setNextNode(node2);
+        }
+
+        if (node2Prev == null) {
+            list.head = node1;
+        } else {
+            node2Prev.setNextNode(node1);
+        }
+
+        Node temp = node1.getNextNode();
+        node1.setNextNode(node2.getNextNode());
+        node2.setNextNode(temp);
+    }
+
     public static void main(String[] args) {
         // Linked List
         LinkedList seasons = new LinkedList();
@@ -34,5 +87,14 @@ public class Main {
 
         subway.removeByData("Times Square");
         subway.printList();
+
+        // Swap
+        LinkedList testList = new LinkedList();
+        for (int i = 0; i <= 10; i++) {
+            testList.addToTail(String.valueOf(i));
+        }
+        testList.printList();
+        swapNodes(testList, "2", "9");
+        testList.printList();
     }
 }
