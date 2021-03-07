d01db184a85be31025151d0300f7583b25dcc87d
augustine <augustino0890@gmail.com>
2021-03-07 19:31:57 +0900

    Doubly Linked Lists

--- technical/src/com/augustine/linds/Main.java
+++ technical/src/com/augustine/linds/Main.java
@@ -15,5 +15,24 @@ public class Main {
 
         seasons.removeHead();
         seasons.printList();
+
+        // DoublyLinkedList
+        DoublyLinkedList subway = new DoublyLinkedList();
+        subway.addToHead("Times Square");
+        subway.addToHead("Grand Central");
+        subway.addToHead("Central Park");
+        subway.printList();
+
+        subway.addToTail("Penn Station");
+        subway.addToTail("Wall Street");
+        subway.addToTail("Brooklyn Bridge");
+        subway.printList();
+
+        subway.removeHead();
+        subway.removeTail();
+        subway.printList();
+
+        subway.removeByData("Times Square");
+        subway.printList();
     }
 }
--- technical/src/com/augustine/linds/Node.java
+++ technical/src/com/augustine/linds/Node.java
@@ -3,6 +3,7 @@ package com.augustine.linds;
 public class Node {
     public String data;
     private Node next;
+    private Node previous;
 
     public Node(String data) {
         this.data = data;
@@ -13,7 +14,15 @@ public class Node {
         this.next = node;
     }
 
+    public void setPreviousNode(Node node) {
+        this.previous = node;
+    }
+
     public Node getNextNode() {
         return this.next;
+    }
+
+    public Node getPreviousNode() {
+        return this.previous;
     }
 }
