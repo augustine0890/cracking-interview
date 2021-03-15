f313dc17cce663bc4f31b48c95c6726d62f438fc
augustine <augustino0890@gmail.com>
2021-03-16 00:18:29 +0900

    fraudulent activity notifications

--- ds-python/ex/fraudulent_activity.py
+++ ds-python/ex/fraudulent_activity.py
@@ -1,9 +1,9 @@
 # Problem: https://www.hackerrank.com/challenges/fraudulent-activity-notifications/
-import bisect as bs
+import bisect
 
 def index(a, x):
     'Locate the leftmost value exactly equal to x'
-    i = bs.bisect_left(a, x)
+    i = bisect.bisect_left(a, x)
     if i != len(a) and a[i] == x:
         return i
     raise ValueError
@@ -37,11 +37,58 @@ def activityNotifications(expenditure, days):
         
         #del heap[heap.index(log[to_del])]
         del heap[index(heap, expenditure[to_del])]
-        bs.insort(heap, expenditure[ind])
+        bisect.insort(heap, expenditure[ind])
         to_del += 1
             
     return times
 
+# 2nd approach
+from collections import deque
+import statistics
+"""
+def median(arr):
+    n = len(arr)
+    mid = n//2
+
+    if n % 2:
+        return sorted(arr)[mid]
+    return sum(sorted(arr)[mid-1:mid+1])/2
+
+def activityNotifications(expenditure, days):
+    times = 0
+    a = expenditure[:days]
+    a = deque(a)
+    print(a)
+    m = statistics.median(a)
+    for i in range(days, len(expenditure)-1):
+        if expenditure[i] >= 2*m:
+            times += 1
+        a.popleft()
+        a.append(expenditure[i])
+        print(a)
+        m = statistics.median(a)
+    return times
+"""
+def med(arr, d, m):
+    if d % 2 == 0:
+        return sum(arr[m - 1:m + 1]) / 2
+    return arr[int(m)]
+
+def activityNotifications(expenditure, d):
+    n = len(expenditure)
+    arr = sorted(expenditure[:d])
+    m = d//2
+    times = 0
+        
+    for i in range(d, n):
+        if expenditure[i] >= 2*med(arr, d, m):        
+            times += 1
+
+        print(expenditure[i-d])
+        del arr[bisect.bisect_left(arr, expenditure[i-d])]
+        bisect.insort(arr, expenditure[i])
+    
+    return times
 
 if __name__ == "__main__":
     results = activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5)
