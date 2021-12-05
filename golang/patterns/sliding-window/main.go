07562735c61b5c4c1110a011ded9895ef09286b5
augustine <augustino0890@gmail.com>
2021-12-05 15:26:31 +0900

    Average subarrays of size K

--- golang/patterns/sliding-window/main.go
+++ golang/patterns/sliding-window/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 // Given an array, find the average of all contiguous subarrays of size 'K' in it.
-func findAverages(K int, arr []int) []float64 {
+// Time complexity: O(N*K) --> O(N)
+func findAveragesBF(K int, arr []int) []float64 {
 	var result []float64
 	for i := 0; i <= len(arr)-K; i++ {
 		var sum float64
@@ -16,7 +17,26 @@ func findAverages(K int, arr []int) []float64 {
 	return result
 }
 
+func findAverages(K int, arr []int) []float64 {
+	res := make([]float64, len(arr)-K+1)
+	windowSum, windowStart := 0.0, 0
+
+	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
+		windowSum += float64(arr[windowEnd])
+		if windowEnd >= K-1 {
+			res[windowStart] = windowSum / float64(K)
+			windowSum -= float64(arr[windowStart])
+			windowStart++
+		}
+	}
+
+	return res
+}
+
 func main() {
+	resBF := findAveragesBF(5, []int{1, 3, 2, 6, -1, 4, 1, 8, 2})
+	fmt.Println("Averages of subarrays of size K (Burte-Force):", resBF)
 	res := findAverages(5, []int{1, 3, 2, 6, -1, 4, 1, 8, 2})
-	fmt.Println("Averages of subarrays of size K: ", res)
+	fmt.Println("Averages of subarrays of size K:", res)
+
 }
