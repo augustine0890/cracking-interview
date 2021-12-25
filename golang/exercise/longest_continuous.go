5f139a8c946484eb008068a22f30e598c86da5e4
augustine <augustino0890@gmail.com>
2021-12-25 22:22:57 +0900

    cracking-interview: longest continous subsequence

---
+++ golang/exercise/longest_continuous.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func maxLength(arr []int) int {
+	res := 1
+	incr, decr := 1, 1
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1] < arr[i] {
+			incr++
+			if incr > res {
+				res = incr
+			}
+			decr = 1
+		} else {
+			decr++
+			if decr > res {
+				res = decr
+			}
+			incr = 1
+		}
+	}
+	return res
+}
+
+func main() {
+	r := maxLength([]int{1, 5, 3, 2, 1, 3, 4, 5, 6})
+	fmt.Println(r)
+}
